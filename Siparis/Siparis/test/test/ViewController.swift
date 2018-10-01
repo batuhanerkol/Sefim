17f4401070d96f418377e18349092d0fe392d17b
Batuhan Erkol <>
2018-10-01 20:55:15 +0300

    no message

---
+++ Siparis/Siparis/test/test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Batuhan Erkol on 1.10.2018.
+//  Copyright © 2018 Batuhan Erkol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
