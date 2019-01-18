c07fdfa5e6b0d28982ae28b71c2348cd5d0ba6c6
Batuhan Erkol <>
2019-01-18 11:37:52 +0300

    Main.storyboard yeni "TotalHammaddeVC.swift" eklendi, 3 label oluşturuldu. TotalHammaddeVC ile label bağlantıları kuruldu.

---
+++ Siparis/Siparis/Profil/TotalHammaddeVC.swift
@@ -0,0 +1,24 @@
+//
+//  TotalHammaddeVC.swift
+//  Siparis
+//
+//  Created by Batuhan Erkol on 18.01.2019.
+//  Copyright © 2019 Batuhan Erkol. All rights reserved.
+//
+import UIKit
+
+class TotalHammaddeVC: UIViewController {
+
+    @IBOutlet weak var toplamMiktarLabel: UILabel!
+    @IBOutlet weak var hammaddeAdiLabel: UILabel!
+    @IBOutlet weak var secilenUrunAdi: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    // bu sayfaya gelmek için identifier = toTotalHammadde
+    
+
+
+  
+}
